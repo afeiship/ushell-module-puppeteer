3f0a2a081f2d6c9263aa603d8d3fbddba264f19e
feizheng <1290657123@qq.com>
2016-08-02 14:30:12 +0800

    initial

---
+++ src/angular/directive/toggle.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular.module('nx.widget')
+    .directive('nxToggle', function () {
+      return {
+        restrict: 'E',
+        scope: {
+          cssClass: '@',
+          value: '='
+        },
+        controller: ['$scope', function ($scope) {
+          $scope.plus = plus;
+          $scope.minus = minus;
+          $scope.value = $scope.value || 0;
+
+          function plus() {
+            $scope.value++;
+          }
+
+          function minus() {
+            $scope.value--;
+          }
+        }],
+        template: '<div class="nx-widget-toggle {{cssClass}}">' +
+        '<button class="btn plus" ng-click="plus()">+</button>' +
+        '<input class="value" ng-model="value" type="tel">' +
+        '<button class="btn minus" ng-click="minus()">-</button>' +
+        '</div>'
+      };
+
+    });
+
+
+})();
