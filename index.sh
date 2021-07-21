c191b3a4a2084f7760ae86993491f65aa1f64381
afeiship <1290657123@qq.com>
2021-07-21 13:27:25 +0800

    fix: missing export

--- index.sh
+++ index.sh
@@ -4,7 +4,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # Mac OS:
 if [[ $USM_OS_TYPE == 'macos' ]]; then
-  PUPPETEER_EXEC_PATH=${HOME}/github/chrome-mac/Chromium.app/Contents/MacOS/Chromium
+  export PUPPETEER_EXEC_PATH=${HOME}/github/chrome-mac/Chromium.app/Contents/MacOS/Chromium
 fi
 
 unset ROOT_PATH;
