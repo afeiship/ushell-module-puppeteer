2e0f279dcff9ea608284d6157d0fded34779594b
afeiship <1290657123@qq.com>
2021-08-27 16:09:42 +0800

    fix: skip  ppt

--- index.sh
+++ index.sh
@@ -4,7 +4,8 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # Mac OS:
 if [[ $USM_OS_TYPE == 'macos' ]]; then
-  export PUPPETEER_EXEC_PATH=${HOME}/github/chrome-mac/Chromium.app/Contents/MacOS/Chromium
+  export PUPPETEER_EXEC_PATH=${HOME}/github/chrome-mac/Chromium.app/Contents/MacOS/Chromium;
+  export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true;
 fi
 
 unset ROOT_PATH;
